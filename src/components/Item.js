6556b1a13c2024d502d52e6f066ff245842ac9b4
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-04-18 11:44:10 +0300

    complete code update in app.js,item,shoppinglist js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  const handleClick = () => {
+    setIsInCart((prevIsInCart) => !prevIsInCart);
+  };
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
+      <button onClick={handleClick}>
+        {isInCart ? "Remove from Cart" : "Add to Cart"}
+      </button>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
     </li>
   );
 }
